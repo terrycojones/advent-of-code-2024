17730ba629f54c7e37a06eaf87e1643427f44786
terrycojones <terry@jon.es>
2024-12-10 11:08:38 +0100

    Combined two ifs in day-10 solution.

--- day-10/solution.py
+++ day-10/solution.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
+import sys
 import numpy as np
-from utils import readGrid, find
-from icecream import ic
 from collections import defaultdict
 
 
-data = readGrid()
+data = np.array([tuple(map(int, line.strip())) for line in sys.stdin.readlines()])
 counts = np.zeros(data.shape, dtype=int)
 nrows, ncols = data.shape
 reachable = defaultdict(set)
@@ -16,14 +15,17 @@ def count_upward(value, row, col, grid, counts):
     for rowinc, colinc in ((0, -1), (0, 1), (-1, 0), (1, 0)):
         newrow = row + rowinc
         newcol = col + colinc
-        if 0 <= newrow < nrows and 0 <= newcol < ncols:
-            if grid[newrow, newcol] == value + 1:
-                if value == 8:
-                    counts[row, col] += 1
-                    reachable[(row, col)].add((newrow, newcol))
-                else:
-                    counts[row, col] += counts[newrow, newcol]
-                    reachable[(row, col)].update(reachable[(newrow, newcol)])
+        if (
+            0 <= newrow < nrows
+            and 0 <= newcol < ncols
+            and grid[newrow, newcol] == value + 1
+        ):
+            if value == 8:
+                counts[row, col] += 1
+                reachable[(row, col)].add((newrow, newcol))
+            else:
+                counts[row, col] += counts[newrow, newcol]
+                reachable[(row, col)].update(reachable[(newrow, newcol)])
 
 
 for value in range(8, -1, -1):
@@ -40,5 +42,5 @@ for row in range(nrows):
             result1 += len(reachable[(row, col)])
             result2 += counts[row, col]
 
-print("Solution 1", result1)
-print("Solution 2", result2)
+print("Solution 1:", result1)
+print("Solution 2:", result2)
