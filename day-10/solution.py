2e2fdcc1f2f389a7fbafc2a0ad2a93e736cca36a
terrycojones <terry@jon.es>
2024-12-10 11:04:00 +0100

    Added day 10

---
+++ day-10/solution.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import numpy as np
+from utils import readGrid, find
+from icecream import ic
+from collections import defaultdict
+
+
+data = readGrid()
+counts = np.zeros(data.shape, dtype=int)
+nrows, ncols = data.shape
+reachable = defaultdict(set)
+
+
+def count_upward(value, row, col, grid, counts):
+    for rowinc, colinc in ((0, -1), (0, 1), (-1, 0), (1, 0)):
+        newrow = row + rowinc
+        newcol = col + colinc
+        if 0 <= newrow < nrows and 0 <= newcol < ncols:
+            if grid[newrow, newcol] == value + 1:
+                if value == 8:
+                    counts[row, col] += 1
+                    reachable[(row, col)].add((newrow, newcol))
+                else:
+                    counts[row, col] += counts[newrow, newcol]
+                    reachable[(row, col)].update(reachable[(newrow, newcol)])
+
+
+for value in range(8, -1, -1):
+    for row in range(nrows):
+        for col in range(ncols):
+            if data[row, col] == value:
+                count_upward(value, row, col, data, counts)
+
+result1 = result2 = 0
+
+for row in range(nrows):
+    for col in range(ncols):
+        if data[row, col] == 0:
+            result1 += len(reachable[(row, col)])
+            result2 += counts[row, col]
+
+print("Solution 1", result1)
+print("Solution 2", result2)
